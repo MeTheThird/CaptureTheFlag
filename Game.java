69272d04ab7ecef476c46c993a5608a17c60fa21
fatmeany <prabha.krishna@gmail.com>
2014-11-08 18:26:41 -0800

    Create Game.java

---
+++ Game.java
@@ -0,0 +1,29 @@
+package CTF;
+
+import java.util.ArrayList;
+
+public class Game {
+	//List of all the team objects.
+	ArrayList<Team> teams;
+	
+	public Game() {
+		teams = new ArrayList <Team> ();
+	}
+	
+	public void addTeam(Team t) {
+		teams.add(t);
+	}
+
+	public void draw() {
+		for (Team t : teams) {
+			t.draw();
+		}
+	}
+
+	public void move() {
+		for (Team t : teams) {
+			t.move();
+		}
+	}
+
+}
